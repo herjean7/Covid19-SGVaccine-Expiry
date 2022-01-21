9dbd369fc790d91ba5ba217a9e89c1a41210dc8e
wong hejun <wonghejun@outlook.com>
2022-01-21 23:50:33 +0800

    Edited text

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="SG Covid Vaccine Expiry Checker" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Date of last primary series dose : 
         </p>
       </main>
 
