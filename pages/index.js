5cdd736b321ab955373c1efdeaec5f5a90a81b8a
wong hejun <wonghejun@outlook.com>
2022-01-21 23:52:39 +0800

    Updated Title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SG Covid Vaccine Expiry Checker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
